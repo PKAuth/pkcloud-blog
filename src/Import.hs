fe0fba2bf003195302aedcc2f8b4142e00fcba88
James Parker <dev@jamesparker.me>
2015-11-02 14:17:17 -0500

    update for lts-3.0

--- src/Import.hs
+++ src/Import.hs
@@ -8,5 +8,5 @@ import PKCloud.Import as Export
 import PKCloud.Blog.Core as Export
 import PKCloud.Blog.Routes as Export
 
-type Handler a = (PKCloudBlog master) => HandlerT PKCloudBlogApp (HandlerT master IO) a
+type Handler a = forall master . (PKCloudBlog master) => HandlerT PKCloudBlogApp (HandlerT master IO) a
 -- type Handler a = (MonadHandler m, MonadBaseControl IO m) => HandlerT PKCloudBlogApp (HandlerT master m) a
