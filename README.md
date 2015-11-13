f01cdbac7e0528613138e041c5b451f25cc17e77
James Parker <dev@jamesparker.me>
2015-11-13 03:25:59 -0500

    javascript for slug

---
+++ README.md
@@ -0,0 +1,5 @@
+External dependencies
+=====================
+
+- [Javascript slug library](http://pid.github.io/speakingurl/)
+- [Javascript jQuery library](https://jquery.com/)
--- src/PKCloud/Blog/Handler/New.hs
+++ src/PKCloud/Blog/Handler/New.hs
@@ -2,6 +2,8 @@ module PKCloud.Blog.Handler.New (getPKCloudBlogNewR, postPKCloudBlogNewR) where
 
 import Import
 
+import Text.Julius (rawJS)
+
 data FormData = FormData {
       _formDataTitle :: PostTitle
     , _formDataSlug :: PostLink
@@ -14,16 +16,41 @@ generateHTML (formW, formEnc) = lift $ pkcloudDefaultLayout [whamlet|
     |]
 
 renderNewForm :: MasterForm FormData
-renderNewForm = renderBootstrap3 BootstrapBasicForm $ FormData
-    <$> areq textField titleSettings Nothing
-    <*> areq textField slugSettings Nothing
+renderNewForm markup = do
+    titleId <- newFormIdent
+    slugId <- newFormIdent
+    (res, widget') <- renderBootstrap3 BootstrapBasicForm (FormData
+        <$> areq textField (withId titleId titleSettings) Nothing
+        <*> areq textField (withId slugId slugSettings) Nothing
+      ) markup
+    let widget = do
+            toWidget [julius|
+                (function() {
+                    var cachedValue = null;
+                    $('##{rawJS titleId}').on('change keydown paste input mouseup', function() {
+                        var value = $(this).val()
+                        if (value != cachedValue) {
+                            cachedValue = value;
+                            $('##{rawJS slugId}').val( getSlug( value));
+                        }
+                    });
+                })();
+            |]
+            widget'
+    return (res, widget)
 
     where
         titleSettings = withAutofocus $ withPlaceholder "Title" $ 
             bfs ("Title" :: Text)
 
-        slugSettings = withPlaceholder "Permalink" $ 
+        slugSettings = disable $ withPlaceholder "Permalink" $ 
             bfs ("Permalink" :: Text)
+
+        withId i setting = setting {fsId = Just i}
+
+        disable setting = 
+            let attrs = fsAttrs setting in
+            setting {fsAttrs = ("disabled","disabled"):attrs}
 
 getPKCloudBlogNewR :: Handler site post edit Html
 getPKCloudBlogNewR = do
